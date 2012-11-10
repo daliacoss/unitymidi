95c14a1df185b7916da9dc16c32c2c7e26c967d6
Michael Bartnett <michael@bartnett.com>
2012-11-10 18:25:09 -0500

    Finished midi frameworknessy fun

--- Assets/midion/MinionSpawner.cs
+++ Assets/midion/MinionSpawner.cs
@@ -3,7 +3,27 @@ using System.Collections;
 
 public class MinionSpawner : MonoBehaviour
 {
-	public MinionSpawner()
+    public MidiPlayer midiPlayer;
+
+    void Start()
     {
+        midiPlayer.OnNoteOn += ProcessNoteOn;
+        midiPlayer.OnNoteOff += ProcessNoteOff;
+        midiPlayer.OnOtherMidiEvent += ProcessOtherMidiMessage;
+    }
+
+    private void ProcessNoteOn(int channel, int note, int velocity)
+    {
+
+    }
+
+    private void ProcessNoteOff(int channel, int note)
+    {
+
+    }
+
+    private void ProcessOtherMidiMessage(MidiEvent midiEvent)
+    {
+
     }
 }
