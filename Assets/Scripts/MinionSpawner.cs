380cec7d25b650e3bf3f0b2f0f74a829857ac33d
Michael Bartnett <michael@bartnett.com>
2012-11-10 19:04:58 -0500

    Building out minion spawner

--- Assets/Scripts/MinionSpawner.cs
+++ Assets/Scripts/MinionSpawner.cs
@@ -4,27 +4,35 @@ using CSharpSynth.Midi;
 
 public class MinionSpawner : MonoBehaviour
 {
+    public Transform spawnPoint;
     public MidiPlayer midiPlayer;
+    public int noteOnsBetweenEachSpawn;
+
+    public GameObject[] prefabsToSpawn;
 
     void Start()
     {
         midiPlayer.OnNoteOn += ProcessNoteOn;
         midiPlayer.OnNoteOff += ProcessNoteOff;
         midiPlayer.OnOtherMidiEvent += ProcessOtherMidiMessage;
+        midiPlayer.Play();
     }
 
+    int noteOnCounter = 0;
     private void ProcessNoteOn(int channel, int note, int velocity)
     {
-
+        if (noteOnCounter % noteOnsBetweenEachSpawn == 0) {
+            if (prefabsToSpawn.Length >= 1)
+                Object.Instantiate(prefabsToSpawn[0], spawnPoint.position, Quaternion.identity);
+        }
+        noteOnCounter++;
     }
 
     private void ProcessNoteOff(int channel, int note)
     {
-
     }
 
     private void ProcessOtherMidiMessage(MidiEvent midiEvent)
     {
-
     }
 }
