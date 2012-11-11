528ee4fbac51e5904b3d2a837d070c93ddeb8fb2
Michael Bartnett <michael@bartnett.com>
2012-11-11 13:29:51 -0500

    Player health implemented, add the component to the player in the scene

---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,63 @@
+using UnityEngine;
+using System;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public float maxHealth = 100f;
+    public float healthRechargeDelay = 3f;
+    public float healthRechargePerSecond = 30f;
+    public LayerMask layerToTakeDamageFrom = 1 << 9;
+    public float CurrentHealth;
+    public Vector2 healthBarPosition = new Vector2(5, 5);
+    public int healthBarThickness = 20;
+    public float healthBarWidthScale = 1f;
+    public Texture healthbarImage;
+
+    private float lastHitTime = -1f;
+    private bool recharging = false;
+    private Rect healthBar;
+
+    void Awake()
+    {
+        CurrentHealth = maxHealth;
+        healthBar = new Rect(healthBarPosition.x, healthBarPosition.y, maxHealth, healthBarThickness);
+    }
+
+    void OnCollisionEnter(Collision collisionInfo)
+    {
+        Debug.LogWarning("HIT!!!!");
+        if ((( 1 << collisionInfo.gameObject.layer) & layerToTakeDamageFrom) != 0) {
+            var damage = collisionInfo.gameObject.GetComponent<DamageDealer>();
+            if (damage != null) {
+                lastHitTime = Time.time;
+                CurrentHealth -= damage.DealDamage();
+                recharging = false;
+            } else {
+                Debug.LogError("Collided with Baddo, but Baddo not deal damage");
+            }
+        }
+    }
+
+    void Update()
+    {
+        if (!recharging) {
+            if (Time.time - lastHitTime >= healthRechargeDelay) {
+                recharging = true;
+            }
+        } else {
+            if (CurrentHealth < maxHealth) {
+                CurrentHealth = Math.Min(CurrentHealth + healthRechargePerSecond * Time.deltaTime, maxHealth);
+            }
+        }
+    }
+
+    void OnGUI()
+    {
+        healthBar.width = CurrentHealth * healthBarWidthScale;
+        if (healthbarImage != null) {
+            GUI.DrawTexture(healthBar, healthbarImage);
+        } else {
+            Debug.LogError("You should assign the player health component a healthbar image");
+        }
+    }
+}
